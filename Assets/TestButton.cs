7db665f0ea641a89cb71eeed99a410efd58bc71d
侑弥 小島 <alm798coco@gmail.com>
2022-05-24 17:34:29 +0900

    trail

---
+++ Assets/TestButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButton : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Vector3 _vector = new Vector3(-96.66f, 1.4f, 99.79f);
+        clsCreateEmptyObj.Create("trail", _vector, Quaternion.identity);
+        GameObject _trail = GameObject.Find("trail");
+        _trail.AddComponent<TrailRenderer>();
+        TrailRenderer _trailRenderer = _trail.GetComponent<TrailRenderer>();
+        Material mat = (Material)Resources.Load("CFX3_GlowSpike ADD");
+        _trailRenderer.material = mat;
+
+        GameObject _robot = GameObject.Find("Robot1");
+        Transform _transform = clsSetParent.SearchTransform("V6S051", _robot.transform);
+        _trail.transform.parent = _transform;
+    }
+}
