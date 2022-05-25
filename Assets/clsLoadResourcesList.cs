712c3274595c7da782104f05c11debd9c45e62e1
侑弥 小島 <alm798coco@gmail.com>
2022-05-25 11:45:53 +0900

    リソース一覧

--- Assets/clsLoadResourcesList.cs
+++ Assets/clsLoadResourcesList.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class clsLoadResourcesList : MonoBehaviour
 {
@@ -12,12 +13,19 @@ public class clsLoadResourcesList : MonoBehaviour
     {
         GameObject[] _robotArray = Resources.LoadAll<GameObject>("");
 
+        List<string> _optionList = new List<string>();
+
         using (StreamWriter sw = new StreamWriter(m_writeCsvPath, false))
         {
             foreach (GameObject _robo in _robotArray)
             {
                 sw.WriteLine(_robo.name);
+                _optionList.Add(_robo.name);
             }       
         }
+
+        Dropdown _dd = GameObject.Find("Dropdown").GetComponent<Dropdown>();
+        _dd.ClearOptions();
+        _dd.AddOptions(_optionList);
     }
 }
