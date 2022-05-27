60a34c6382e8b283cb1e2e675136a247605cac6d
侑弥 小島 <alm798coco@gmail.com>
2022-05-27 17:54:06 +0900

    座標系追加

--- Assets/clsCameraController.cs
+++ Assets/clsCameraController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +7,16 @@ using UnityEngine.SocialPlatforms;
 [RequireComponent(typeof(Camera))]
 public class clsCameraController : MonoBehaviour
 {
-    public string m_key = "1";
+    public string m_key;
 
     [SerializeField, Range(0.1f, 10f)]
-    private float m_wheelSpeed = 2f; // �z�C�[�� �g�k���x
+    private float m_wheelSpeed = 2f;
 
     [SerializeField, Range(0.1f, 10f)]
-    private float m_moveSpeed = 0.3f; // ���N���b�N�h���b�O �ړ����x
+    private float m_moveSpeed = 0.3f;
 
     [SerializeField, Range(0.1f, 10f)]
-    private float m_rotateSpeed = 0.3f; // �E�N���b�N�h���b�O ��]���x
+    private float m_rotateSpeed = 0.3f;
 
     private Vector3 m_preMousePos;
 
@@ -38,7 +38,6 @@ public class clsCameraController : MonoBehaviour
             if (m_key == Input.inputString)
             {
                 _moveFlg = true;
-                
                 GetComponent<Camera>().cullingMask = -1;
             }
             else if (m_keyCodeList.Contains(Input.inputString))
@@ -59,7 +58,6 @@ public class clsCameraController : MonoBehaviour
 
     private void MouseUpdate()
     {
-        // ���炩���N���b�N�������_�ł̃}�E�X�J�n�ʒu
         if (Input.GetMouseButtonDown(0) ||
            Input.GetMouseButtonDown(1) ||
            Input.GetMouseButtonDown(2))
@@ -67,10 +65,8 @@ public class clsCameraController : MonoBehaviour
             m_preMousePos = Input.mousePosition;
         }
 
-        // �����_�ł̃}�E�X�ʒu������
         MouseDrag(Input.mousePosition);
 
-        // �z�C�[�����쎞�̊g�k
         float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
         if (scrollWheel != 0.0f)
         {
@@ -80,17 +76,13 @@ public class clsCameraController : MonoBehaviour
 
     private void MouseWheel(float delta)
     {
-        // �g�k Z���W�Ƀ}�E�X�z�C�[���Ɗ��x�𔽉f
         transform.position += transform.forward * delta * m_wheelSpeed;
     }
 
     private void MouseDrag(Vector3 mousePos)
     {
-        // �}�E�X�J�n�ʒu�ƌ����_�ł̈ʒu�̍�
         Vector3 diff = mousePos - m_preMousePos;
 
-        // �����L���Ȓl�łȂ��ꍇ
-        // ���̃x�N�g����Vector3�̌���Ȃ�0�ɋ߂����̒l��菬�����ꍇ
         if (diff.magnitude < Vector3.kEpsilon)
         {
             //return;
@@ -98,32 +90,19 @@ public class clsCameraController : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-            // ���N���b�N�h���b�O�ňړ�
-
-            // ��(���F���])�ɒ��O�ƍ��̃t���[���Ԃ̌o�ߎ��ԂƊ��x�𔽉f
-            // Translate�͌��݂̈ʒu���瑊�ΓI�Ȉʒu�ֈړ�����(���݂̈ʒu����������ړ�����)
             transform.Translate(-diff * Time.deltaTime * m_moveSpeed);
         }
         else if (Input.GetMouseButton(1))
         {
-            // �E�N���b�N�h���b�O�ŉ�]
-
-            // X��Y���ŉ�]���̈ʒu��XY�t�ɂȂ邽�ߓ���ւ���
             CameraRotate(new Vector2(-diff.y, diff.x) * m_rotateSpeed);
         }
 
-        // �}�E�X�J�n�ʒu���X�V
         m_preMousePos = mousePos;
     }
 
     public void CameraRotate(Vector2 angle)
     {
-        // �J�����㉺��]
-        // �J�������g�𒆐S��X����x�x��]
         transform.RotateAround(transform.position, transform.right, angle.x);
-
-        // �J�������E��]
-        // �J�������g�𒆐S��Y����y�x��]
         transform.RotateAround(transform.position, Vector3.up, angle.y);
     }
 }
--- Assets/clsFollowCamera.cs
+++ Assets/clsFollowCamera.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class clsFollowCamera : MonoBehaviour
 {
-    private GameObject m_camera;
+    public GameObject m_camera;
+    public string m_key;
+    private bool m_moveFlg = false;
     private List<string> m_keyCodeList = new List<string> { "1", "2", "3", "4" };
-    private Vector3 _addPosFront;
-    private Vector3 _addPosTop;
-    private Vector3 _addPosRight;
+    private float m_forward = 30.0f;
+    private float m_up = 5.0f;
+    private float m_Right = 11.0f;
+    private Vector3 m_addPosFront;
+    private Vector3 m_addPosUp;
+    private Vector3 m_addPosRight;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+        m_keyCodeList.Remove(m_key);
     }
 
     // Update is called once per frame
@@ -21,37 +27,38 @@ public class clsFollowCamera : MonoBehaviour
     {
         if (Input.anyKeyDown)
         {
-            if (m_keyCodeList.Contains(Input.inputString))
+            if (m_key == Input.inputString)
             {
-                transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-
-                switch (Input.inputString)
-                {
-                    case "1":
-                        m_camera = GameObject.Find("Camera1");
-                        break;
-                    case "2":
-                        m_camera = GameObject.Find("Camera2");
-                        break;
-                    case "3":
-                        m_camera = GameObject.Find("Camera3");
-                        break;
-                    case "4":
-                        m_camera = GameObject.Find("Camera4");
-                        break;
-                    default:
-                        break;
-                }
-
-                //_addPos = m_camera.transform.position;
+                m_moveFlg = true;
+            }
+            else if (m_keyCodeList.Contains(Input.inputString))
+            {
+                m_moveFlg = false;
             }
         }
+    }
 
-        if (m_camera != null)
+    private void FixedUpdate()
+    {
+        if (m_moveFlg)
+        {            
+            m_addPosFront = new Vector3(m_camera.transform.forward.x * m_forward
+                    , m_camera.transform.forward.y * m_forward
+                    , m_camera.transform.forward.z * m_forward);
+            m_addPosUp = new Vector3(m_camera.transform.up.x * m_up
+                    , m_camera.transform.up.y * m_up
+                    , m_camera.transform.up.z * m_up);
+            m_addPosRight = new Vector3(m_camera.transform.right.x * m_Right
+                    , m_camera.transform.right.y * m_Right
+                    , m_camera.transform.right.z * m_Right);
+
+            transform.position = m_camera.transform.position + m_addPosFront + m_addPosUp + m_addPosRight;
+
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+        else
         {
-
-            transform.position = m_camera.transform.forward;
-            transform.rotation = m_camera.transform.rotation;
+            transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
         }
     }
 }
